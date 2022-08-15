1ba44a7055505c62ad7c47bf29f3337a407fa119
Ani Mihaylova <anivm.webdev@gmail.com>
2022-08-15 18:26:04 -0400

    creates basic header structure

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,15 @@
 import './Header.scss'
+import logo from "../../assets/images/BrainFlix-logo.svg"
+import profile from "../../assets/images/Mohan-muruge.jpg"
 
 function Header () {
     return(
-        <header>
-            This is the header
+        <header className="header">
+            <a href="/" className="header__logo">
+                <img src={logo}  alt="Brainflix Logo" />
+            </a>
+            <input type="text" className="header__search" />
+            <img src={profile} alt="Mohan in profile" className="header__profile-pic" />
         </header>
     )
 }
