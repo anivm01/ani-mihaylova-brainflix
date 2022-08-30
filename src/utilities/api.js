d015c4dead0c8b6e58b1a01d589c2d3c2a00f5ba
Ani Mihaylova <anivm.webdev@gmail.com>
2022-08-29 20:06:22 -0400

    clean up api utilities file to use new api data

--- src/utilities/api.js
+++ src/utilities/api.js
@@ -1,12 +1,9 @@
 import axios from "axios"
-const API_KEY = "?api_key=16d78b4b-b137-4077-901e-223ffe2a7355"
-const API_URL = "https://project-2-api.herokuapp.com"
-
 
 export const fetchVideos = () => {
-    return axios.get(`${API_URL}/videos${API_KEY}`)
+    return axios.get(`http://localhost:8000/videos/`)
 }
 
 export const fetchVideoByID = (id) => {
-    return axios.get(`${API_URL}/videos/${id}${API_KEY}`)
+    return axios.get(`http://localhost:8000/videos/${id}`)
 }
